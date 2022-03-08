c23d42699808ba6ed08def27c4c5bb72de2bbcee
游威 <1647373860@qq.com>
2022-03-08 10:59:46 +0800

    nacos,gateway整合运行成功

--- nacos/src/main/java/com/demo/nacos/NacosApplication.java
+++ nacos/src/main/java/com/demo/nacos/NacosApplication.java
@@ -1,8 +1,10 @@
 package com.demo.nacos;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@NacosPropertySource(dataId = "application.yml", autoRefreshed = true)
 @SpringBootApplication
 public class NacosApplication {
 
