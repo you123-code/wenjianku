e539d129e3398f6edc180633d037b93c8ca00b10
游威 <1647373860@qq.com>
2022-04-13 11:35:30 +0800

    优化代码

--- testdatasource/src/main/java/com/demo/testdatasource/controller/RequestController.java
+++ testdatasource/src/main/java/com/demo/testdatasource/controller/RequestController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * <p>
  * 项目申报信息 前端控制器
@@ -23,8 +27,13 @@ public class RequestController {
     @Autowired
     private RequestService requestService;
     @GetMapping("/getUser")
-    public Request getUser(String id){
-        return requestService.getById(id);
+    public List<Request> getUser(String id){
+        List<Request> requests  = new ArrayList<>();
+        List<String> strings = Arrays.asList(id.split(","));
+        strings.forEach(item->{
+           Request request = requestService.getById(item);
+           requests.add(request);
+        });
+        return requests;
     }
-
 }
