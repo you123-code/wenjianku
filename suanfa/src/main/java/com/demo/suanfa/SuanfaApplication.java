5a079489aed692eb4e666f249058847a925fd3a9
游威 <1647373860@qq.com>
2022-04-25 17:26:20 +0800

    添加算法模块

---
+++ suanfa/src/main/java/com/demo/suanfa/SuanfaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.suanfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuanfaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuanfaApplication.class, args);
+    }
+
+}
---
+++ suanfa/src/test/java/com/demo/suanfa/SuanfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.suanfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuanfaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
